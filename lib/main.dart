bcad084569cbe38c855101258de69fec1eb4d54c
pranave2003 <pranav2003a10@gmail.com>
2025-04-10 12:35:57 +0530

    firebase config success

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pet_clinic/Admin/Admin_login.dart';
 import 'package:pet_clinic/User/User_doctordetails.dart';
 import 'package:pet_clinic/User/User_homepage.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
