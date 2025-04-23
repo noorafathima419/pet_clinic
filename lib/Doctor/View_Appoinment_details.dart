e59512645335b6d3a5eacc6a54e11f79a7dc8f6b
HP <noorafathimafeb26@gmail.com>
2025-04-23 13:15:39 +0530

    23/04/2025

--- lib/Doctor/View_Appoinment_details.dart
+++ lib/Doctor/View_Appoinment_details.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -17,68 +18,82 @@ class _AddVaccinationState extends State<AddVaccination> {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: 4,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: EdgeInsets.only(left: 30.w, right: 30.w),
-                  child: Card(
-                    elevation: 3,
-                    margin: EdgeInsets.symmetric(vertical: 10),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Container(
-                      padding: EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: Color(0xffCF6A6AF0E4E4),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            height: 50.w,
-                            width: 50.w,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.rectangle,
-                              color: Colors.grey[400],
-                            ),
-                            child: Icon(Icons.person,
-                                size: 40.sp, color: Colors.white),
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("Appoinment_details")
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (!snapshot.hasData) {
+                  return Center(child: Text("no data found"));
+                }
+                var doct = snapshot.data!.docs;
+                return ListView.builder(
+                  itemCount: doct.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(left: 30.w, right: 30.w),
+                      child: Card(
+                        elevation: 3,
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Container(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            color: Color(0xffCF6A6AF0E4E4),
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                          SizedBox(width: 16.w),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("User name",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 16.sp,
-                                        fontWeight: FontWeight.w600)),
-                                Text("pet name",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 14.sp,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w600)),
-                                Text(
-                                  "pet type",
-                                  style: GoogleFonts.inter(
-                                      fontSize: 14.sp,
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w600),
+                          child: Row(
+                            children: [
+                              Container(
+                                height: 50.w,
+                                width: 50.w,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                  color: Colors.grey[400],
                                 ),
-                                Text("Gender",
-                                    style: GoogleFonts.inter(
-                                        fontSize: 14.sp,
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.w600)),
-                                SizedBox(height: 4.h),
-                              ],
-                            ),
+                                child: Icon(Icons.person,
+                                    size: 40.sp, color: Colors.white),
+                              ),
+                              SizedBox(width: 16.w),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(doct[index]["owner_name"],
+                                        style: GoogleFonts.inter(
+                                            fontSize: 16.sp,
+                                            fontWeight: FontWeight.w600)),
+                                    Text(doct[index]["name"],
+                                        style: GoogleFonts.inter(
+                                            fontSize: 14.sp,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w600)),
+                                    Text(
+                                      doct[index]["pet_type"],
+                                      style: GoogleFonts.inter(
+                                          fontSize: 14.sp,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(doct[index]["gender"],
+                                        style: GoogleFonts.inter(
+                                            fontSize: 14.sp,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w600)),
+                                    SizedBox(height: 4.h),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
+                    );
+                  },
                 );
               },
             ),
