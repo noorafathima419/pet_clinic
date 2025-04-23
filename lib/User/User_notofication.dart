087627deccb656aee7ab84c507e9b1cbd4e85a82
HP <noorafathimafeb26@gmail.com>
2025-04-23 11:52:52 +0530

    23/04/2025

--- lib/User/User_notofication.dart
+++ lib/User/User_notofication.dart
@@ -14,69 +14,72 @@ class UserNotofication extends StatefulWidget {
 class _UserNotoficationState extends State<UserNotofication> {
   @override
   Widget build(BuildContext context) {
-    return  FutureBuilder(future: FirebaseFirestore.instance.collection("admin_notification").doc().get(),
-    builder: (context, snapshot) {
-    if (snapshot.connectionState == ConnectionState.waiting) {
-    return const Center(child: CircularProgressIndicator());
-    }
-    if (snapshot.hasError) {
-    return Center(child: Text("user found"));
-    }
-    if (!snapshot.hasData || snapshot.data == null) {
-    return Center(child: Text("no user data found"));
-    }
-    final user = snapshot.data!.data() as Map<String, dynamic>;
-    return Scaffold(
-      backgroundColor: Color(0xffFFFFFF),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
-        child: ListView.builder(
-          itemCount: user.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.only(bottom: 10.h),
-              child: Container(
-                height: 200.h,
-                width: 200.w,
-                child: Card(
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.r),
-                  ),
-                  child: ListTile(
-                    title: Text(
-                      "Notification",
-                      style: GoogleFonts.poppins(
-                        fontSize: 16.sp,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    subtitle: Padding(
-                      padding:  EdgeInsets.only(top: 100.h),
-                      child: Text(
-                        user[index]["Date"],
-                        style: GoogleFonts.poppins(
-                          fontSize: 14.sp,
-                          color: Colors.grey[700],
+    return FutureBuilder(
+        future: FirebaseFirestore.instance
+            .collection("admin_notification")
+            .doc()
+            .get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text("user found"));
+          }
+          if (!snapshot.hasData || snapshot.data == null) {
+            return Center(child: Text("no user data found"));
+          }
+          final user = snapshot.data!.data() as Map<String, dynamic>;
+          return Scaffold(
+            backgroundColor: Color(0xffFFFFFF),
+            body: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 10.h),
+              child: ListView.builder(
+                itemCount: user.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: 10.h),
+                    child: Container(
+                      height: 200.h,
+                      width: 200.w,
+                      child: Card(
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        child: ListTile(
+                          title: Text(
+                            "Notification",
+                            style: GoogleFonts.poppins(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          subtitle: Padding(
+                            padding: EdgeInsets.only(top: 100.h),
+                            child: Text(
+                              user[index]["Date"],
+                              style: GoogleFonts.poppins(
+                                fontSize: 14.sp,
+                                color: Colors.grey[700],
+                              ),
+                            ),
+                          ),
+                          trailing: Text(
+                            user[index]["Time"],
+                            style: GoogleFonts.poppins(
+                              fontSize: 14.sp,
+                              color: Colors.grey[700],
+                            ),
+                          ),
                         ),
                       ),
                     ),
-                    trailing: Text(
-                      user[index]["Time"],
-                      style: GoogleFonts.poppins(
-                        fontSize: 14.sp,
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                  ),
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
-      ),
-    );
-    }
-    );
+            ),
+          );
+        });
   }
 }
